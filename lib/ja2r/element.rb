ff7578aec148b095c500c8aa6f6b99a9c180e3b3
Konstantin Munteanu <munteanu@fidor.com>
2019-02-19 11:48:51 +0100

    Support serializers

--- lib/ja2r/element.rb
+++ lib/ja2r/element.rb
@@ -1,40 +1,53 @@
 module JA2R
   class Element
-    def initialize(payload)
-      @id = payload['id']
-      @type = payload['type']
-      @attributes = (payload['attributes'] || {}).with_indifferent_access
-      @meta = (payload['meta'] || {}).with_indifferent_access
-      @links = (payload['links'] || {}).with_indifferent_access
-      @relationships = payload['relationships'] ? convert_relationship(payload['relationships']) : {}
+    def initialize(origin_data)
+      @origin_data = origin_data.with_indifferent_access
+      @relationships = origin_data['relationships'] ? convert_relationship(origin_data['relationships']) : {}
     end
 
-    attr_reader :id, :type, :attributes, :relationships
+    attr_reader :origin_data, :relationships
 
     def attribute(key)
-      @attributes[key]
+      origin_data.dig 'attributes', key
+    end
+
+    def attributes
+      origin_data['attributes']
+    end
+
+    def id
+      origin_data['id']
     end
 
     def link(key)
-      @links[key]
+      origin_data.dig 'links', key
     end
 
     def meta(key)
-      @meta[key]
+      origin_data.dig 'meta', key
+    end
+
+    # Support serializers
+    alias read_attribute_for_serialization public_send
+
+    alias serializable_hash origin_data
+
+    def type
+      origin_data['type']
     end
 
     private
 
-    def method_missing(method, *args)
-      return attributes[method] if attributes&.key? method
-      return relationships[method] if relationships&.key? method
+    def method_missing(symbol, *args)
+      return attributes[symbol] if attributes&.key? symbol
+      return relationships[symbol] if relationships&.key? symbol
 
       super
     end
 
     def respond_to_missing?(symbol, include_all = false)
-      return true if attributes&.key?(symbol.to_s)
-      return true if relationships&.key?(symbol.to_s)
+      return true if attributes&.key?(symbol)
+      return true if relationships&.key?(symbol)
 
       super
     end
